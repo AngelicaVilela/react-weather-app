cc6155317a0464a2d3ab2ff4a702b9661795298e
AngelicaVilela <71622206+AngelicaVilela@users.noreply.github.com>
2021-02-04 22:29:26 -0500

    Link to github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <p>This app was coded by <a href="https://github.com/AngelicaVilela" rel="noreferrer" target="_blank">Angélica Vilela</a></p>
     </div>
   );
 }
