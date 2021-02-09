f498c84b6cd7ae858e9842f45c4eca2047a677a7
AngelicaVilela <71622206+AngelicaVilela@users.noreply.github.com>
2021-02-09 10:01:20 -0500

    No more whitespace

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -4,7 +4,7 @@ import "./Weather.css";
 export default function FormattedDate(props){
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let months = ["Jan", "Feb", "March","Apr","May","Jun","Jul","Aug","Sep","Oct", "Nov", "Dec"];
-    let day = days [props.date.getDay()];
+    let day = days[props.date.getDay()];
     let hours = props.date.getHours();
     let minutes = props.date.getMinutes();
     let month = months[props.date.getMonth()];
